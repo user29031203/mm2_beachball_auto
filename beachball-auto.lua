1b5719ab5b097240ddf0dbfafc991564581d70bd
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-10-13 07:13:57 +0300

    Update beachball-auto.lua

--- beachball-auto.lua
+++ beachball-auto.lua
@@ -88,8 +88,6 @@ pcall(function()
 		coinsCollected = getCoinCount()
         if coinsCollected >= 40 then
             if char and char:FindFirstChild("Humanoid") then
-                char.Humanoid.Health = 0
-                print("Character reset, coin counter reset to 0")
                 -- Wait for character to respawn
                 repeat
                     task.wait(0.5)
