ed2f251ff8d1c3099534ff818e130aadeb079e6f
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-12-02 19:28:29 +0300

    Update wrong-match-handler.lua

--- wrong-match-handler.lua
+++ wrong-match-handler.lua
@@ -7,6 +7,17 @@ local CONS_INFO = loadstring(game:HttpGet(CONS_INFO_URL))()
 local ServerApi = loadstring(game:HttpGet(CONS_INFO.URLS.SERVER_MANAGER_URL))()
 local LeaderboardApi = loadstring(game:HttpGet(CONS_INFO.URLS.LEADERBOARD_LIB_URL))()
 
+
+-- teleportatin support 
+local TeleportQueue = queue_on_teleport 
+if not TeleportQueue then
+    TeleportQueue = (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
+end
+if not TeleportQueue then
+    warn("Executor TeleportQueue function not found. Cannot queue command for next server.")
+    return
+end
+
 local function CheckAndHandleMatching()
     local leaderstats = LocalPlayer:WaitForChild("leaderstats", generalTimeout)
     
