d227bace95df56a6ca018fdde045552cef29dca5
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-12-02 18:31:44 +0300

    Create constants.lua

---
+++ constants.lua
@@ -0,0 +1,8 @@
+local consInfo = {
+    hosterName = "306a2e5cd_2",
+    joinerName = "306a2e5cd_1",
+    hosterId = 9359470613, -- A2
+    joinerId = 9359433164,
+    mySecretKey = "21bdef3f9b5a7e65db63ea9ac3d9f34a",
+    generalTimeout = 10
+}
