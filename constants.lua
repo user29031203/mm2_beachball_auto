110f23f98dc7abf3ab5d9d467fa14d0951f7cfaa
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-12-02 18:41:27 +0300

    Update constants.lua

--- constants.lua
+++ constants.lua
@@ -4,5 +4,13 @@ local consInfo = {
     hosterId = 9359470613, -- A2
     joinerId = 9359433164,
     mySecretKey = "21bdef3f9b5a7e65db63ea9ac3d9f34a",
-    generalTimeout = 10
+    generalTimeout = 10,
+    URLS = {
+        TELEPORT_HANDLER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/teleport-handler.lua",
+        DWEETR_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/dweetr-lib.lua",
+        SERVER_MANAGER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/server-manager.lua",
+        TELEPORT_HANDLER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/teleport-handler.lua",
+        LEADERBOARD_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/leaderboard-lib.lua",
+        MOVEMENT_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/movement-lib.lua"
+    }
 }
