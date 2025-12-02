8416f0c3496394bb0c950305bcc9ad2f34df1e3a
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-12-02 19:57:04 +0300

    Update wrong-match-rejoiner.lua

--- wrong-match-rejoiner.lua
+++ wrong-match-rejoiner.lua
@@ -3,17 +3,24 @@ local CONS_INFO = loadstring(game:HttpGet(CONS_INFO_URL))()
 
 local LeaderboardApi = loadstring(game:HttpGet(CONS_INFO.URLS.LEADERBOARD_LIB_URL))()
 
+-- teleportatin support 
+local TeleportQueue = queue_on_teleport 
+if not TeleportQueue then
+    TeleportQueue = (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
+end
+if not TeleportQueue then
+    warn("Executor TeleportQueue function not found. Cannot queue command for next server.")
+    return
+end
+
 local function shareJobId() 
     ---
 end
 
 local status = LeaderboardApi.IsDuoMatched(CONS_INFO.hosterName, CONS_INFO.joinerName)
-print(status)
 
 if not status then
-    print(MyId)
-    if MyId == CONS_INFO.joinerId then
-        -- share jobid via dweetr then join to hoster 
-        print("DEBUGGER A1")
-    end
+    print("DEBUGGER A1")
 end
+
+pcall(TeleportQueue, "return")
