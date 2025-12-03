9b572c0dbcbadfc88674ca83b885c844a09d571c
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-03 21:14:00 +0300

    Update wrong-match-rejoiner.lua

--- wrong-match-rejoiner.lua
+++ wrong-match-rejoiner.lua
@@ -8,8 +8,8 @@ local CONS_INFO = loadstring(game:HttpGet(CONS_INFO_URL))()
 -- external libs
 local LeaderboardApi = loadstring(game:HttpGet(CONS_INFO.URLS.LEADERBOARD_LIB_URL))()
 local ServerApi = loadstring(game:HttpGet(CONS_INFO.URLS.SERVER_MANAGER_URL))()
---[[local DweetLib = loadstring(game:HttpGet(CONS_INFO.URLS.DWEETR_LIB_URL))()
-local Comm = DweetLib.new(CONS_INFO.mySecretKey)]]
+local DweetLib = loadstring(game:HttpGet(CONS_INFO.URLS.DWEETR_LIB_URL))()
+local Comm = DweetLib.new(CONS_INFO.mySecretKey)
 
 -- teleportatin support 
 local TeleportQueue = queue_on_teleport 
@@ -24,17 +24,47 @@ end
 local status = LeaderboardApi.IsDuoMatched(CONS_INFO.hosterName, CONS_INFO.joinerName)
 local hosterJobId
 
-if MyId == CONS_INFO.hosterId then
-    hosterJobId = ServerApi.GetCurrentServerInfo().JobId
+--ServerApi.GetCurrentServerInfo().JobId
+
+local function SendJobId()
+    print("Sending data to dweetr.io...")
+    return Comm:Send({
+        Status = "Working",
+        JobId = game.JobId,
+        Time = 0
+    })
 end
 
-if MyId == CONS_INFO.hosterId and status == false then        -- ← CHANGE THIS TO ALT1'S USERID
+local function ReadJobId()
+	--task.wait(2.5) -- fix it by perfect 30 seconds limited waiting
+    local data, createdTime = Comm:GetLatest()
+
+    if data then
+        print("SUCCESS! Read back JobId:", data.JobId, "\nCreated Time:", createdTime)
+	else
+        warn("Failed to read data.")
+		return false
+    end
+    
+    return data
+end
+
+status = false
+
+if MyId == CONS_INFO.hosterId and status == false then       -- ← CHANGE THIS TO ALT1'S USERID
     -- send jobid through dweetr
     print("IM HOST!")
+    local success, msg = SendJobId()
+	print("Send Result:", msg) 
 elseif MyId == CONS_INFO.joinerId and status == false then
     -- receive jobid through dweetr 
     print("IM JOINER!")
-    ServerApi.JoinServerById(CONS_INFO.duelsPlaceId, hosterJobId)
+    task.wait(3)
+    local ReadedData = ReadJobId() 
+	if ReadedData then 
+    	ServerApi.JoinServerById(CONS_INFO.duelsPlaceId, ReadedData.JobId)
+	end
 end 
 
+local MAIN_SCRIPT = "loadstring(game:HttpGet('" .. CONS_INFO.URLS.MAIN_CODE_URL .. "'))()"
 pcall(TeleportQueue, "return")
