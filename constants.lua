3c86578f74ccc38617f10a0f33463ed1a9af046c
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-03 22:48:00 +0300

    Update constants.lua

--- constants.lua
+++ constants.lua
@@ -11,10 +11,10 @@ do
 end
 
 local consInfo = {
-    hosterName = "306a2e5cd_2",
-    joinerName = "306a2e5cd_1",
-    hosterId = 9359470613, -- A2
-    joinerId = 9359433164,
+    hosterName = "X0010010",
+    joinerName = "306a2e5cd_2",
+    hosterId = 7573604968, 
+    joinerId = 9359470613,
     mySecretKey = "21bdef3f9b5a7e65db63ea9ac3d9f34a",
     duelsPlaceId = 12360882630,
     generalTimeout = 4.5,
