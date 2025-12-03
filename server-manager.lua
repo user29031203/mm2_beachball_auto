a305ea91866e08259a1e21218413f6e4b30487ab
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-03 16:08:10 +0300

    Update server-manager.lua

--- server-manager.lua
+++ server-manager.lua
@@ -2,15 +2,12 @@ local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-local ServerManager = {
-    duelsPlaceId = 12360882630
-}
-
 function ServerManager.GetCurrentServerInfo()
     return { PlaceId = game.PlaceId, JobId = game.JobId }
 end
 
 function ServerManager.JoinServerById(placeId, jobId)
+    
     TeleportService:TeleportToPlaceInstance(placeId, jobId)
 end
 
