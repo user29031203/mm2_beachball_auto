974a6db573fa08681f04ca69bbec16e78b912d77
user29031203 <97709e18397e4ae26e71101@protonmail.com>
2025-12-03 07:28:31 +0300

    Update wrong-match-handler.lua

--- wrong-match-handler.lua
+++ wrong-match-handler.lua
@@ -43,7 +43,7 @@ if MyId == CONS_INFO.joinerId then
     local WrongMatchRejoiner = "loadstring(game:HttpGet('" .. CONS_INFO.URLS.WRONG_MATCH_REJOINER_URL .. "'))()"  
     pcall(TeleportQueue, WrongMatchRejoiner)
 else
-    pcall(TeleportQueue, print("Status: STABLE AND TESTING A56"))
+    pcall(TeleportQueue, print("Status: STABLE AND TESTING A58"))
 end
 
 return CheckAndHandleMatching()
