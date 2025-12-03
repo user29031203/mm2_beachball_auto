e534262196fec37aee47fab6009393a61328e84a
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-03 07:53:37 +0300

    Update constants.lua

--- constants.lua
+++ constants.lua
@@ -1,3 +1,14 @@
+local OWNER = "user29031203"
+local REPO  = "LegendZero"      
+local BRANCH = "main"         
+
+local BASE_RAW_URL
+do
+    local api = ("https://api.github.com/repos/%s/%s/commits/%s"):format(OWNER, REPO, BRANCH)
+    local sha = game:HttpGet(api, true):match('"sha":"([a-f0-9]+)"') or BRANCH
+    BASE_RAW_URL = ("https://raw.githubusercontent.com/%s/%s/%s/"):format(OWNER, REPO, sha)
+end
+
 local consInfo = {
     hosterName = "306a2e5cd_2",
     joinerName = "306a2e5cd_1",
@@ -6,15 +17,25 @@ local consInfo = {
     mySecretKey = "21bdef3f9b5a7e65db63ea9ac3d9f34a",
     generalTimeout = 4.5,
     URLS = {
-        TELEPORT_HANDLER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/teleport-handler.lua",
-        DWEETR_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/dweetr-lib.lua",
-        SERVER_MANAGER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/server-manager.lua",
-        TELEPORT_HANDLER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/teleport-handler.lua",
-        LEADERBOARD_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/leaderboard-lib.lua",
-        MOVEMENT_LIB_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/movement-lib.lua",
-        WRONG_MATCH_HANDLER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/wrong-match-handler.lua",
-        WRONG_MATCH_REJOINER_URL = "https://raw.githubusercontent.com/user29031203/LegendZero/refs/heads/main/wrong-match-rejoiner.lua"
+        TELEPORT_HANDLER_URL     = "teleport-handler.lua",
+        DWEETR_LIB_URL           = "dweetr-lib.lua",
+        SERVER_MANAGER_URL       = "server-manager.lua",
+        LEADERBOARD_LIB_URL      = "leaderboard-lib.lua",
+        MOVEMENT_LIB_URL         = "movement-lib.lua",
+        WRONG_MATCH_HANDLER_URL  = "wrong-match-handler.lua",
+        WRONG_MATCH_REJOINER_URL = "wrong-match-rejoiner.lua",
     }
-}
+} 
+
+local function raw(file)
+    local sha = game:HttpGet(("https://api.github.com/repos/%s/%s/commits/%s"):format(OWNER, REPO, BRANCH), true)
+                :match('"sha":"([a-f0-9]+)"') or BRANCH
+    
+    return ("https://raw.githubusercontent.com/%s/%s/%s/%s"):format(OWNER, REPO, sha, file:gsub("^/*", ""))
+end
+
+for key, fileName in pairs(URLS) do
+    URLS[key] = BASE_RAW_URL .. fileName
+end
 
 return consInfo
