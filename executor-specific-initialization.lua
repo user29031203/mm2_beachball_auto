83585f6c5602d33276f9ce5597aa24e484943d57
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-04 09:23:37 +0300

    Update executor-specific-initialization.lua

--- executor-specific-initialization.lua
+++ executor-specific-initialization.lua
@@ -1,4 +1,5 @@
 -- teleportatin support api for running a code on join/teleport
+
 local TeleportQueue = queue_on_teleport 
 if not TeleportQueue then
     TeleportQueue = (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
@@ -7,3 +8,5 @@ if not TeleportQueue then
     warn("Executor TeleportQueue function not found. Cannot queue command for next server.")
     return
 end
+
+return TeleportQueue
