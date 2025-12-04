47ca960956954d3f9c2bf7da98feb04ebb2a199b
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-04 09:29:58 +0300

    Update wrong-match-rejoiner.lua

--- wrong-match-rejoiner.lua
+++ wrong-match-rejoiner.lua
@@ -13,15 +13,9 @@ local ServerApi = loadstring(game:HttpGet(CONS_INFO.URLS.SERVER_MANAGER_URL))()
 local DweetLib = loadstring(game:HttpGet(CONS_INFO.URLS.DWEETR_LIB_URL))()
 local Comm = DweetLib.new(CONS_INFO.mySecretKey)
 
--- teleportatin support 
-local TeleportQueue = queue_on_teleport 
-if not TeleportQueue then
-    TeleportQueue = (syn and syn.queue_on_teleport) or (fluxus and fluxus.queue_on_teleport)
-end
-if not TeleportQueue then
-    warn("Executor TeleportQueue function not found. Cannot queue command for next server.")
-    return
-end
+-- initialize executor specific required APIs
+local TeleportQueue = loadstring(game:HttpGet(CONS_INFO.URLS.EXECUTOR_API_INIT_URL))()
+if not TeleportQueue then return end --check if it loaded without problems
 
 local status = LeaderboardApi.IsDuoMatched(CONS_INFO.hosterName, CONS_INFO.joinerName)
 
