5d534388fc6baa1c7caa84a3726050a344c7c605
X <97709e18397e4ae26e71101@protonmail.com>
2025-12-05 02:04:29 +0300

    Update lobby-refresher.lua

--- lobby-refresher.lua
+++ lobby-refresher.lua
@@ -2,7 +2,7 @@ local CONS_INFO_URL = "https://raw.githubusercontent.com/user29031203/LegendZero
 local CONS_INFO = loadstring(game:HttpGet(CONS_INFO_URL))()
 
 -- external libs 
-local ServerApi = loadstring(game:HttpGet(CONS_INFO.URLS.SERVER_MANAGER_URL))()
+local ServerApi = CONS_INFO.Load(CONS_INFO.URLS.SERVER_MANAGER_URL)
 
 -- initialize executor specific required APIs
 local TeleportQueue = loadstring(game:HttpGet(CONS_INFO.URLS.EXECUTOR_API_INIT_URL))()
